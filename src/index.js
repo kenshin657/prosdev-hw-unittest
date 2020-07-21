fe009d1c2cc3e586d39ecc25e6f7e964523096fa
KTagle <kyletagle727@gmail.com>
2020-07-21 17:58:47 +0800

    did stuff :)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 const functions = {
   gradeClassifier: (grade) => {
     // TODO: Write code here
+	if(grade > 94.99 && grade <= 100)
+		return 'O';
+	if(grade > 89.99 && grade <= 94.99)
+		return 'V';
+	if(grade > 84.99 && grade <= 89.99)
+		return 'G';
+	if(grade > 79.99 && grade <= 84.99)
+		return 'S';
+	if(grade > 74.99 && grade <= 79.99)
+		return 'N';
+	if(grade <= 74.99)
+		return 'D';
   }
 }
 
--- src/index.test.js
+++ src/index.test.js
@@ -1,12 +1,31 @@
 const { gradeClassifier } = require('./index');
 
 // TODO: Write unit tests here
-describe('Scenario', () => {
-  it('When ..., then ...', () => {
-    // Arrange
-
-    // Act
-
-    // Assert
+describe('Computing Letter Grade', () => {
+  it('Should Return Appropriate Letter Grade', () => {
+	  
+			// Arrange
+	var test1 = 89.50
+	var test2 = 60.00
+	var test3 = 100
+	var test4 = 75.65
+	var test5 = 83.33
+	var test6 = 94.99
+			// Act
+	var result1 = gradeClassifier(test1)
+	var result2 = gradeClassifier(test2)
+	var result3 = gradeClassifier(test3)
+	var result4 = gradeClassifier(test4)
+	var result5 = gradeClassifier(test5)
+	var result6 = gradeClassifier(test6)
+			// Assert
+	expect(result1).toEqual('G')
+	expect(result2).toEqual('D')
+	expect(result3).toEqual('O')
+	expect(result4).toEqual('N')
+	expect(result5).toEqual('S')
+	expect(result6).toEqual('V')
+	  
+    
   });
 });
